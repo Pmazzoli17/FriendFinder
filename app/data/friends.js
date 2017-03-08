cd184abff63e0c2af5563a165eef77c2afdf2d1f
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-08 12:19:03 -0500

    final changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,98 +1,98 @@
 var friends = [
   {
-  	"name":"Ahmed",
-  	"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  	"name":"Harry Potter",
+  	"photo":"https://s-media-cache-ak0.pinimg.com/originals/e2/50/a2/e250a240c9a6ef39edc4fe4be85f47ca.jpg",
   	"scores":[
-	    5,
-	    1,
-	    4,
-        4,
-	    5,
-	    1,
-	    2,
-	    5,
-	    4,
-	    1
+	    "5",
+	    "1",
+	    "4",
+        "4",
+	    "5",
+	    "1",
+	    "2",
+	    "5",
+	    "4",
+	    "1"
     ]
   },
   {
-  	"name": "Jacob Deming",
-	"photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+  	"name": "Ron Weasley",
+	"photo": "https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg",
 	"scores": [
-	 	4,
-		2,
-		5,	
-		1,
-		3,
-		2,
-		2,
-		1,
-		3,
-		2
+	 	"4",
+		"2",
+		"5",
+		"1",
+		"3",
+		"2",
+		"2",
+		"1",
+		"3",
+		"2"
 	]
 },
 {
-	"name": "Jeremiah Scanlon",
-	"photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+	"name": "Hermione Granger",
+	"photo": "https://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poster.jpg",
 	"scores": [
-		5,
-		2,
-		2,
-		2,
-		4,
-		1,
-		3,
-		2,
-		5,
-		5
+		"5",
+		"2",
+		"2",
+		"2",
+		"4",
+		"1",
+		"3",
+		"2",
+		"5",
+		"5"
 	]
 },
 {
-	"name": "Louis T. Delia",
-	"photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+	"name": "Lord Voldemort",
+	"photo": "https://img.buzzfeed.com/buzzfeed-static/static/2016-03/6/15/campaign_images/webdr08/how-lord-voldemort-are-you-g2wy-2-6896-1457296977-1_dblbig.jpg",
 	"scores": [
-		3,
-		3,
-		4,
-		2,
-		2,
-		1,
-		3,
-		2,
-		2,
-		3
+		"3",
+		"3",
+		"4",
+		"2",
+		"2",
+		"1",
+		"3",
+		"2",
+		"2",
+		"3"
 	]
 },
 {
-	"name": "Lou Ritter",
-	"photo": "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+	"name": "Albus Dumbledore",
+	"photo": "https://ladygeekgirl.files.wordpress.com/2014/01/albus-dumbledore.jpg",
 	"scores": [
-		4,
-		3,
-		4,
-		1,
-		5,
-		2,
-		5,
-		3,
-		1,
-		4
+		"4",
+		"3",
+		"4",
+		"1",
+		"5",
+		"2",
+		"5",
+		"3",
+		"1",
+		"4"
 	]
 },
 {
-	"name": "Jordan Biason",
-	"photo": "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+	"name": "Severus Snape",
+	"photo": "http://vignette2.wikia.nocookie.net/harrypotter/images/e/e1/Severus_Snape_Profile.JPG/revision/latest/scale-to-width-down/145?cb=20081103135333",
 	"scores": [
-		4,
-		4,
-		2,
-		3,
-		2,
-		2,
-		3,
-		2,
-		4,
-		5
+		"4",
+		"4",
+		"2",
+		"3",
+		"2",
+		"2",
+		"3",
+		"2",
+		"4",
+		"5"
 	]
   }
 	
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,30 @@
 var path = require("path");
-
+//Route
 module.exports = function (app) {
 
-	app.get("/survey", function(req, res) {
-		res.sendFile(path.join(__dirname + "/../public/survey.html"));
-	});
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    });
 
-	app.use(function(req, res) {
-		res.sendFile(path.join(__dirname + "/../public/home.html"));
-	});
-	
+    app.use(function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
+    });
+
 
 }
+
+
+// var path = require("path");
+
+// module.exports = function (app) {
+
+// 	app.get("/survey", function(req, res) {
+// 		res.sendFile(path.join(__dirname + "/../public/survey.html"));
+// 	});
+
+// 	app.use(function(req, res) {
+// 		res.sendFile(path.join(__dirname + "/../public/home.html"));
+// 	});
+	
+
+// }
